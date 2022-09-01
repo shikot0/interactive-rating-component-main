4e9392c9b76bab535e33ebb79fef80b8cb44ea3a
shikoto <1sh1m1da1@gmail.com>
2022-09-01 01:37:23 +0000

    Edited README file

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const buttons = document.querySelectorAll('.rating-button');
 const ratingCard = document.getElementById('rating-card');
 const submitBtn = document.getElementById('submit-btn');
 const userSelection = document.getElementById('rating-selected')
-const modal = document.querySelector('.modal')
-console.log(modal)
+const modal = document.querySelector('.modal'); 
+
+
 buttons.forEach(button => { 
     button.addEventListener('click', () => {
         buttons.forEach(button => {
@@ -18,7 +19,6 @@ buttons.forEach(button => {
 submitBtn.addEventListener('click', () => {
     for(let button of buttons) {
         if(button.classList.contains('selected')) {
-            console.log(button.value) 
             userSelection.innerText = button.value;
             ratingCard.classList.add('hide');
             modal.setAttribute('id','show'); 
